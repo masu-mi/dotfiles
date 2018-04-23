7d660bca5400ed7feb135ecde769bdf1c4af3a9d
Kanai Masumi <masumi.net@gmail.com>
2018-04-23 17:53:43 +0900

    Bugfix .fzf.bash

--- .fzf.bash
+++ .fzf.bash
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/masumi/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/masumi/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  export PATH="$PATH:${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/masumi/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && . "${HOME}/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/masumi/.fzf/shell/key-bindings.bash"
+. "${HOME}/.fzf/shell/key-bindings.bash"
 
