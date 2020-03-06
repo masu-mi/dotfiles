e901ef617810f2a8ba0e63053f5326be7e550848
Kanai Masumi <masumi.net@gmail.com>
2020-03-06 22:11:26 +0900

    Add snip-permutations

---
+++ vim/template/go/snip-permutations.go
@@ -0,0 +1,35 @@
+{{_cursor_}}
+func permutations(l int) chan []int {
+	ch := make(chan []int)
+	go func() {
+		dfsPermutations(0, make([]bool, l), []int{}, func(perm []int) bool {
+			p := make([]int, len(perm))
+			copy(p, perm)
+			ch <- p
+			return false
+		})
+		close(ch)
+	}()
+	return ch
+}
+
+func dfsPermutations(pos int, used []bool, perm []int, atLeaf func(perm []int) (halt bool)) (halt bool) {
+	l := len(used)
+	if pos == l {
+		if atLeaf(perm) {
+			return true
+		}
+	}
+
+	for i := 0; i < l; i++ {
+		if used[i] {
+			continue
+		}
+		used[i] = true
+		if dfsPermutations(pos+1, used, append(perm, i), atLeaf) {
+			return true
+		}
+		used[i] = false
+	}
+	return false
+}
