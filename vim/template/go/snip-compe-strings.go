c61311bb22e2e5847af4a652e2f23f9cc2de0260
Kanai Masumi <masumi.net@gmail.com>
2020-03-08 21:03:19 +0900

    Update template and snippets

---
+++ vim/template/go/snip-compe-strings.go
@@ -0,0 +1,51 @@
+{{_cursor_}}
+
+func reverseString(str string) string {
+	buf := bytes.NewBufferString("")
+	writeReverse(buf, str)
+	return buf.String()
+}
+
+func reverseStrings(strs ...string) (rev []string) {
+	for i := len(strs); i > 0; i-- {
+		rev = append(rev, strs[i-1])
+	}
+	return
+}
+
+func writeStrings(w io.Writer, strs ...string) {
+	ch := make(chan string)
+	go func() {
+		for _, s := range strs {
+			ch <- s
+		}
+		close(ch)
+	}()
+	writeWithChanString(w, ch)
+}
+
+func writeFlattenStrings(r io.Writer, strss ...[]string) {
+	ch := make(chan string)
+	go func() {
+		for _, strs := range strss {
+			for _, s := range strs {
+				ch <- s
+			}
+		}
+		close(ch)
+	}()
+	writeWithChanString(r, ch)
+}
+
+func writeWithChanString(r io.Writer, strs chan string) {
+	for s := range strs {
+		r.Write(([]byte)(s))
+	}
+}
+
+func writeReverse(r io.Writer, str string) {
+	runes := []rune(str)
+	for i := len(runes); i > 0; i-- {
+		r.Write(([]byte)(string(runes[i-1])))
+	}
+}
