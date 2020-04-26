1ceeec16e6090f20d91ee54884404bb617d2a311
Kanai Masumi <masumi.net@gmail.com>
2020-04-26 22:52:53 +0900

    Add snip-compe-dp

---
+++ vim/template/go/snip-compe-dp.go
@@ -0,0 +1,21 @@
+{{_cursor_}}
+const (
+	inf  = math.MaxInt32
+	ninf = math.MinInt32
+)
+
+func changeToMin(v *int, cand int) (updated bool) {
+	if *v > cand {
+		*v = cand
+		updated = true
+	}
+	return updated
+}
+
+func changeToMax(v *int, cand int) (updated bool) {
+	if *v < cand {
+		*v = cand
+		updated = true
+	}
+	return updated
+}
