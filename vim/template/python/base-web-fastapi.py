ffcfad4fda08e950867504135d7bbe2cb619829c
Kanai Masumi <masumi.net@gmail.com>
2020-06-04 08:09:22 +0900

    Rename template

--- vim/template/python/base-web-fastapi.py
+++ vim/template/python/base-web-fastapi.py
@@ -1,6 +1,7 @@
 # vim: fileencoding=utf-8
 
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +12,10 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+def main():
+    uvicorn.run(app, host='0.0.0.0', port=8000)
+
+if __name__ == '__main__':
+    main()
