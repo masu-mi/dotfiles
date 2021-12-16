582d77e63a7e775a71a72c6b37896d2832324bab
Kanai Masumi <masumi.net@gmail.com>
2021-12-16 13:17:54 +0900

    feat: submodule update --init in init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,6 +4,8 @@ top=$(dirname $0)/..
 cd $top
 echo "[INFO] Start init script, pwd: $(pwd)"
 
+git submodule update --init --recursive
+
 if [ -f "$HOME/.bashrc" -a ! -L "$HOME/.bashrc" ]; then
   echo mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
   mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
