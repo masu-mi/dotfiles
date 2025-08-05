3989fc53b35ac4982918bd3750b51c9b00b12172
Masumi Kanai <masumi.net@gmail.com>
2025-08-05 22:36:16 +0900

    feat: update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,6 +6,12 @@ echo "[INFO] Start init script, pwd: $(pwd)"
 
 git submodule update --init --recursive
 
+curl https://mise.run | sh
+echo "eval \"\$(/home/masumikanai.linux/.local/bin/mise activate bash)\"" >> ~/.bashrc
+. ~/.bashrc
+
+mise exec --env boot -- make init
+
 if [ -f "$HOME/.bashrc" -a ! -L "$HOME/.bashrc" ]; then
   echo mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
   mv "$HOME/.bashrc" "$HOME/.bashrc.bak"
